ede244f1e2e4a3e77c59c19253b68b1f61405762
Dejan Josifovic <Rajoduo@yahoo.com>
2014-09-17 00:18:31 +0200

    Add repl settings for initialization of repl session.

--- project.clj
+++ project.clj
@@ -3,5 +3,13 @@
                 storing ringtones."
   :url "http://github.com/28/democlojure.git"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comment "Same as Clojure"}
+  :repl-options {
+                 :prompt (fn [ns] (str "democlojure-" ns "=>"))
+                 :welcome (println "Welcome to democlojure")
+                 :init-ns console.console
+                 :init (println "Here is the list of the functions to use: log-in-console, sign-console,
+                              play-console, crop-console, log-out-console")}
   :dependencies [[org.clojure/clojure "1.5.1"]])
